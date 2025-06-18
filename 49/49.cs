76910ebcbcf211b45cd39f06f502114d2d67f2d9
Rachel-Kohn <frkohn60@gmail.com>
2025-06-17 20:27:17 -0400

    HW 49

---
+++ 49/49.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework49 {
+    class Program {
+        static void Main(string[] args) {
+            List<string> colors = new List<string> { "red", "green", "blue" };
+            List<string> patterns = new List<string> { "striped", "checked", "plain" };
+            List<Shirt> shirts = new List<Shirt>();
+
+            foreach (string color in colors) { // Create combinations
+                foreach (string pattern in patterns) {
+                    shirts.Add(new Shirt(color, pattern));
+                }
+            }
+            foreach (Shirt shirt in shirts) {// Print shirts
+                Console.WriteLine(shirt);
+            }
+        }
+    }
+    class Shirt {
+        public string Color { get; set; }
+        public string Pattern { get; set; }
+
+        public Shirt(string color, string pattern) {
+            Color = color;
+            Pattern = pattern;
+        }
+
+        public override string ToString() {
+            return $"{Color}:{Pattern}";
+        }
+    }
+}
