3a4eda24da518be85e207b4b613d19ac24bb424a
Rachel-Kohn <frkohn60@gmail.com>
2025-07-22 12:37:33 -0400

    HW 59

--- 49/obj/Debug/net9.0/49.AssemblyInfo.cs
+++ 49/obj/Debug/net9.0/49.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("49")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+316c1c166255aa597f8b1fe2ab9478709814868c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8d6a42bb7cf0d27c9f987626955d713c1ab3afa")]
 [assembly: System.Reflection.AssemblyProductAttribute("49")]
 [assembly: System.Reflection.AssemblyTitleAttribute("49")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
